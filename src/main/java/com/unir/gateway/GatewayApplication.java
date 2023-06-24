44f81f55fd448000346bc39938986a860c44a1a3
Jesus PM - UNIR <jesus.perezmelero@unir.net>
2023-06-24 23:54:55 +0200

    DOCENCIA-37 Beta Private Networking railway

--- src/main/java/com/unir/gateway/GatewayApplication.java
+++ src/main/java/com/unir/gateway/GatewayApplication.java
@@ -9,6 +9,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class GatewayApplication {
 
 	public static void main(String[] args) {
+		
+		// Obtenemos perfil de ejecucion de variable de entorno. Si no hay, perfil default. 
+		String profile = System.getenv("PROFILE");
+		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
