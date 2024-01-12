9c7d66f37296d4be6cef4fd481200a4cd033f6b4
Jesus PM - UNIR <jesus.perezmelero@unir.net>
2024-01-12 22:16:01 +0100

    Remove Health EP + Disable Actuator

--- src/main/java/com/unir/gateway/GatewayApplication.java
+++ src/main/java/com/unir/gateway/GatewayApplication.java
@@ -3,18 +3,10 @@ package com.unir.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RestController
 public class GatewayApplication {
-
-	@GetMapping("/")
-	public String health() {
-		return "OK";
-	}
 
 	public static void main(String[] args) {
 		
